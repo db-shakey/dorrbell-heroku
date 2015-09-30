7e3accddc75d1f4da0927ab9dc6df7dbf7fb1d54
Christopher Moyle <cmoyle336@gmail.com>
2015-09-30 10:26:11 -0500

    Added Authentication Mechanism

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var sass = require('node-sass');
-var fs = require('fs');
-var bodyParser = require('body-parser');
-var Knex = require('knex');
+var express 		= require('express');
+var passport 		= require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var fs 				= require('fs');
+var Knex 			= require('knex');
+var cookieParser 	= require('cookie-parser');
+var bodyParser 		= require('body-parser');
+var session 		= require('express-session');
 
+
+
+
+//Setup Dabase Pool
 var knex = Knex({
     client: 'pg',
     connection: {
@@ -16,31 +23,80 @@ var knex = Knex({
     }
 });
 
+//Load local access objects
 var contact = require("./server/contact")(knex);
 
-var app = express();
+//Configure Passport
+passport.use(new LocalStrategy(
+	function(username, password, done) {
+		contact.findByUsername(username, function(user, err){
+			console.log(user);
+			if(err){return done(err);}
+			else if(!user){return done(null, false);}
+			else if(user.password__c != password){return done(null, false);}
+			else return done(null, user);
+		});
+	}
+));
 
+passport.serializeUser(function(user, cb) {
+  cb(null, user.sfid);
+});
+
+passport.deserializeUser(function(id, cb) {
+	contact.findBySfid(id, function(user, err){
+		if(err){
+			return cb(err);
+		}else{
+			cb(null, user);
+		}
+	});
+});
+
+
+//Main app
+var app = express();
 app.set('port', (process.env.PORT || 5000));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.get('/contact', function(request, response){
+app.get('/contact', require('connect-ensure-login').ensureLoggedIn(), function(request, response){
 	contact.getAllContacts(function(res){
 		response.send(res);
 	}, function(err){
 		response.send(err);
 	});
+})
+app.get('/login',
+  function(req, res){
+    res.send(401,{ success : false, message : 'authentication failed' });
+  });
+
+app.post('/login',
+  passport.authenticate('local', { failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/contact');
+ });
+
+app.post('/noauth', function(req, res){
+	contact.findByUsername(req.body.username, function(user, err){
+		res.send(user);
+	})
 })
 
 app.listen(app.get('port'), function() {
--- server/contact.js
+++ server/contact.js
@@ -4,6 +4,16 @@ module.exports = function(knex){
  	return {
  		getAllContacts : function(callback){
  			knex.select().table('contact').then(callback);
+ 		},
+ 		findByUsername : function(username, callback){
+ 			return knex.select().table('contact').where('email', username).then(function(records){
+ 				callback(records[0]);
+ 			});
+ 		},
+ 		findBySfid : function(sfid, callback){
+ 			return knex.select().table('contact').where('sfid', sfid).then(function(records){
+ 				callback(records[0]);
+ 			});
  		}
  	}
   
