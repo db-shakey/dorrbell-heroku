2f14db24f7eceda42e5ab33da787b378b7437a12
Christopher Moyle <cmoyle336@gmail.com>
2015-10-19 21:37:03 -0400

    Add Delivery Item endpoint

--- routes/authenticated.js
+++ routes/authenticated.js
@@ -41,8 +41,7 @@ module.exports = function(apiRoutes, conn){
 
 
 				if(where){
-					console.log(where);
-					conn.query("SELECT Id, \
+					var query = "SELECT Id, \
 									Variant_SKU__c, \
 									Name, \
 									Variant_Title__c, \
@@ -56,7 +55,8 @@ module.exports = function(apiRoutes, conn){
 								FROM Variant__c \
 							WHERE " + where + " " +
 							geo +
-							" LIMIT " + limit + " OFFSET " + offset, function(err, queryData){
+							" LIMIT " + limit + " OFFSET " + offset;
+					conn.query(query, function(err, queryData){
 						if(err)
 							onError(err, response);
 						response.json(queryData);			
@@ -108,7 +108,8 @@ module.exports = function(apiRoutes, conn){
 		var text = request.params.searchString;
 		var limit = request.params.limit;
 		var offset = request.params.offset;
-		querySearchResults("FIND {*" + text + "*} IN ALL FIELDS RETURNING Variant__c(Id WHERE Store_Id__c = '" + store + "'), Dorrbell_Product__c(Id)", limit, offset, null, response);
+		var order = "ORDER BY Name DESC";
+		querySearchResults("FIND {*" + text + "*} IN ALL FIELDS RETURNING Variant__c(Id WHERE Store_Id__c = '" + store + "'), Dorrbell_Product__c(Id)", limit, offset, order, response);
 
 	})
 
@@ -122,9 +123,6 @@ module.exports = function(apiRoutes, conn){
 	});
 
 	apiRoutes.post('/update/:sObject', function(request, response){
-
-		
-
 		conn.sobject(request.params.sObject).update([
 			request.body
 		], function(err, rets){
@@ -143,6 +141,52 @@ module.exports = function(apiRoutes, conn){
 			response.status(200).send("Ok");
 		})
 	});
+
+
+	apiRoutes.post('/createDeliveryItem', function(request, response){
+		var deliveryId = request.body.deliveryId;
+		var variantId = request.body.variantId;
+
+		conn.query("SELECT Id, \
+							Barcode__c, \
+							Variant_Price__c, \
+							Variant_Title__c, \
+							Image_URL__c, \
+							Dorrbell_Product__r.Name, \
+							Dorrbell_Product__r.Shopify_Product_Type__c, \
+							Variant_SKU__c, \
+							Store_Id__c \
+						FROM Variant__c \
+						WHERE Id = '" + variantId + "'", 
+		function(err, data){
+			console.log(err);
+			console.log(data);
+			var variant = data.records[0];
+			if(err || !variant){
+				onError(err, response);
+			}else{
+				conn.sobject("Delivery_Item__c").create({
+					"Barcode__c" : variant.Barcode__c,
+					"Image_URL__c" : variant.Image_URL__c,
+					"Online_Sale_Price__c" : variant.Variant_Price__c,
+					"Options__c" : variant.Variant_Title__c,
+					"Product_Name__c" : variant.Dorrbell_Product__r.Name,
+					"Product_Type__c" : variant.Dorrbell_Product__r.Shopify_Product_Type__c,
+					"Related_Store__c" : variant.Store_Id__c,
+					"Related_Variant__c" : variant.Id,
+					"Sku__c" : variant.Variant_SKU__c,
+					"Related_Delivery__c" : deliveryId
+				}, function(error2, ret){
+					if(error2 || !ret.success){
+						onError(error2, response);
+					}else{
+						response.send(ret.Id);
+					}
+				});
+			}
+		});
+		
+	})
 
 	apiRoutes.get("/me", function(request, response){
 		response.send(request.decoded);
