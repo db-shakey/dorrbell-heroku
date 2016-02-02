63cfa787bb9fc45ecbdfb6d68bdfb26fe15db65c
Christopher Moyle <cmoyle336@gmail.com>
2016-02-02 00:55:33 -0500

    no message

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -5,9 +5,9 @@ module.exports = function(route, conn, utils){
       utils.log(err);
       res.status(200).send();
     }
-    //utils.log(req.body);
+    utils.log(req.body);
     var product = req.body;
-
+    var route = req.params.route;
 
 
     //Upsert the main product
@@ -19,7 +19,8 @@ module.exports = function(route, conn, utils){
         Family : product.product_type,
         Publish_Scope__c : product.published_scope,
         Tags__c : product.tags,
-        Name : product.title
+        Name : product.title,
+        Store__r : {External_Id__c : product.vendor}
       }, 'Shopify_Id__c', function(err, rets){if(err) reject(err); else resolve(rets);});
     });
 
@@ -73,6 +74,7 @@ module.exports = function(route, conn, utils){
               Taxable__c : v.taxable,
               Weight__c : v.weight,
               Weight_Unit__c : v.weight_unit,
+              Store__r : {External_Id__c : product.vendor},
               Parent_Product__r : {Shopify_Id__c : v.product_id},
               RecordTypeId : recordTypeId
             })
@@ -99,7 +101,7 @@ module.exports = function(route, conn, utils){
           }
           conn.sobject("Product_Image__c").upsert(piArray, 'Shopify_Id__c', function(err){if(err) errorHandler(err);});
 
-          //Upsert the variant options
+          //Upsert the variant options and price book entries
           var variantArray = new Array();
           var pbeList = new Array();
 
@@ -126,13 +128,16 @@ module.exports = function(route, conn, utils){
                 Shopify_Id__c : product.options[2].id + ':' + v.id,
                 Value__c : v.option3
               });
-            pbeList.push({
-              Product2 : {Shopify_Id__c : v.id},
-              Pricebook2 : {External_Id__c : 'standard'},
+            var pbe = {
               UnitPrice : Number(v.compare_at_price),
               IsActive : true,
               External_Id__c : v.id + ':standard'
-            });
+            }
+            if(route == 'create'){
+              pbe.Product2 = {Shopify_Id__c : v.id};
+              pbe.Pricebook2 = {External_Id__c : 'standard'};
+            }
+            pbeList.push(pbe);
           }
           conn.sobject("PricebookEntry").upsert(pbeList, 'External_Id__c', function(err){if(err) errorHandler(err);});
           conn.sobject("Product_Option__c").upsert(variantArray, 'Shopify_Id__c', function(err){if(err) errorHandler(err);});
@@ -144,26 +149,150 @@ module.exports = function(route, conn, utils){
 
 
 	route.post('/order/:route', function(req, res){
-
-    utils.log(req.body);
-
     var order = req.body;
+    var route = req.params.route;
 
-    conn.sobject("Order").create({
-      BillingStreet : order.billing_address.address1,
-      BillingCity : order.billing_address.city,
-      BillingState : order.billing_address.province,
-      BillingCountry : order.billing_address.country,
-      Browser_IP__c : order.browser_ip,
-      Accepts_Marketing__c : order.buyer_accepts_marketing,
-      Cart_Token__c : order.cart_token,
-      Checkout_Id__c : order.checkout_id,
-      Checkout_Token__c : order.checkout_token,
-      Browser_User_Agent__c : order.client_details.user_agent,
-      Browser_Width__c : order.client_details.browser_width,
-      Browser_Height__c : order.client_details.browser_height,
-      Confirmed__c : order.confirmed
-    });
+    utils.log(order);
+    new Promise(function(resolve, reject){
+      var sfOrder = {
+        BillingStreet : order.billing_address.address1,
+        BillingCity : order.billing_address.city,
+        BillingState : order.billing_address.province,
+        BillingCountry : order.billing_address.country,
+        BillingPostalCode : order.billing_address.zip,
+        BillingLatitude : order.billing_address.latitude,
+        BillingLongitude : order.billing_address.longitude,
+        ShippingStreet : order.shipping_address.address1,
+        ShippingCity : order.shipping_address.city,
+        ShippingState : order.shipping_address.province,
+        ShippingCountry : order.shipping_address.country,
+        ShippingPostalCode : order.shipping_address.zip,
+        ShippingLatitude : order.shipping_address.latitude,
+        ShippingLongitude : order.shipping_address.longitude,
+        Browser_IP__c : order.browser_ip,
+        Accepts_Marketing__c : order.buyer_accepts_marketing,
+        Cart_Token__c : order.cart_token,
+        Checkout_Id__c : order.checkout_id,
+        Checkout_Token__c : order.checkout_token,
+        Browser_User_Agent__c : (order.client_details) ? order.client_details.user_agent : null,
+        Browser_Width__c : (order.client_details) ? order.client_details.browser_width : null,
+        Browser_Height__c : (order.client_details) ? order.client_details.browser_height : null,
+        Confirmed__c : order.confirmed,
+        Shopify_Id__c : order.id,
+        Account : {External_Id__c : 'Shopify'},
+        Status : 'Draft',
+        EffectiveDate : order.created_at,
+        Financial_Status__c : order.financial_status,
+        Fulfillment_Status__c : order.fulfillment_status,
+        Number__c : order.number,
+        Order_Number__c : order.order_number,
+        Token__c : order.token,
+        Total_Weight__c : order.total_weight,
+        Note__c : order.note,
+      };
+      if(order.customer && order.customer.id){
+        sfOrder.BillToContact = {Shopify_Customer_ID__c : order.customer.id};
+        sfOrder.ShipToContact = {Shopify_Customer_ID__c : order.customer.id};
+      }
+
+      if(order.payment_details){
+        sfOrder.Credit_Card_Bin__c = order.payment_details.credit_card_bin;
+        sfOrder.Credit_Card_Company__c = order.payment_details.credit_card_company;
+        sfOrder.Credit_Card_Number__c = order.payment_details.credit_card_number;
+        sfOrder.CVV_Result_Code__c = order.payment_details.cvv_result_code;
+      }
+
+      if(order.note_attributes){
+        Date.prototype.normalize = function(){
+          return new Date(this - this.getTimezoneOffset() * 60000);
+        }
+
+        for(var i in order.note_attributes){
+          var n = order.note_attributes[i];
+          if(n.name == "request_additional_items")
+            sfOrder.Request_Additional_Items__c = (n.value.toLowerCase() == "yes") ? true : false
+          else if(n.name == "local_delivery_request"){
+            var sList =n.value.split(" ");
+            if(sList[7] == "PM")
+              sList[6] = (Number(sList[6].substring(0,1)) + 12) + sList[6].substring(1);
+
+            if(sList[11] == "PM")
+              sList[10] = (Number(sList[10].substring(0,1)) + 12) + sList[10].substring(1);
+
+            sfOrder.In_Home_Try_On_Start__c = new Date(sList[1] + " " + sList[2] + ", " + sList[3] + " " + sList[6] + ":00");
+            sfOrder.In_Home_Try_On_End__c = new Date(sList[1] + " " + sList[2] + ", " + sList[3] + " " + sList[10] + ":00");
+            utils.log(order);
+          }
+        }
+      }
+
+      if(route == 'create'){
+        sfOrder.Pricebook2 = {External_Id__c : 'standard'};
+      }
+
+      conn.sobject("Order").upsert(sfOrder, 'Shopify_Id__c', function(err, ret){
+        if(err)
+          reject(err);
+        else
+          resolve(ret);
+      });
+    }).then(function(){
+      var orderProductList = new Array();
+      var orderStoreList = new Array();
+
+      //Delete the existing order deliveries
+      conn.query("SELECT Id FROM Order_Store__c WHERE Order__r.Shopify_Id__c = '" + order.id + "'", function(err, result){
+        var idList = new Array();
+        for(var i in result.records){
+          idList.push(result.records[i].Id);
+        }
+        conn.sobject("Order_Store__c").del(idList, function(err, rets){
+          for(var i in order.line_items){
+            var li = order.line_items[i];
+            if(li.variant_id){
+              //Create the order item
+              var orderItem = {
+                Quantity : li.quantity,
+                UnitPrice : li.price,
+                Shopify_Id__c : li.id,
+                Description : li.name
+              };
+              if(route == 'create'){
+                orderItem.PricebookEntry = {External_Id__c : li.variant_id + ':standard'};
+                orderItem.Order = {Shopify_Id__c : order.id};
+              }
+              orderProductList.push(orderItem);
+
+              //Create the order store
+              orderStoreList.push({
+                Order__r : {Shopify_Id__c : order.id},
+                Store__r : {External_Id__c : li.vendor},
+                External_Id__c : order.id + ':' + li.vendor
+              });
+            }
+          }
+
+          conn.sobject("OrderItem").upsert(orderProductList, 'Shopify_Id__c', function(err, ret){
+            if(err)
+              utils.log(err);
+            else {
+              res.status(200).send();
+            }
+          });
+          conn.sobject("Order_Store__c").upsert(orderStoreList, 'External_Id__c', function(err, ret){
+            if(err)
+              utils.log(err);
+            else {
+              res.status(200).send();
+            }
+          });
+        });
+      });
+    }, function(err){
+      utils.log(err);
+    })
+
+
 
 
     res.status(200).send();
