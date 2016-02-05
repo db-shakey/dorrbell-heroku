057aa93bee00aded53e9292e50c52c9cae8a09ac
Christopher Moyle <cmoyle336@gmail.com>
2016-02-05 00:43:46 -0500

    no message

---
+++ routes/shopify.js
@@ -0,0 +1,66 @@
+module.exports = function(utils){
+  var apiKey = '12ad97558a61e66e2b4bde6dd8f97cd9';
+  var password = 'e465022f2fbf924b05f710f403758345';
+  var http = require('https');
+
+  return {
+    getVariantMetafields : function(variantId){
+      return new Promise(function(resolve, reject){
+        var req = http.get({
+          host : 'homefit.myshopify.com',
+          path : '/admin/variants/' + variantId + '/metafields.json',
+          auth : apiKey + ':' + password
+        }, function(response){
+          var body = '';
+          response.on('data', function(d){
+            body += d;
+          });
+          response.on('end', function(){
+            resolve({
+              variantId : variantId,
+              metafields : JSON.parse(body)
+            });
+          });
+          response.on('error', reject);
+        });
+        req.on('error', function(e) {
+          utils.log(e);
+        });
+      })
+    },
+
+    getProductMetafields : function(productId){
+      return new Promise(function(resolve, reject){
+        var req = http.get({
+          host : 'homefit.myshopify.com',
+          path : '/admin/products/' + productId + '/metafields.json',
+          auth : apiKey + ':' + password
+        }, function(response){
+
+          var body = '';
+          response.on('data', function(d){
+            body += d;
+          });
+          response.on('end', function(){
+            resolve(JSON.parse(body));
+          });
+          response.on('error', reject);
+        });
+
+        req.on('error', function(e) {
+          utils.log(e);
+        });
+      });
+    },
+
+    metaFilter : function(metaList, key){
+      var value = metaList.filter(function(obj){
+        return obj.key == key;
+      });
+      if(value && value.length > 0)
+        return value[0].value;
+      else
+        return null;
+    }
+  }
+};
--- routes/webhooks.js
+++ routes/webhooks.js
@@ -1,45 +1,79 @@
 module.exports = function(route, conn, utils){
 
+
+  /**************************
+	 * Product
+	 *************************/
   route.post('/product/:route', function(req, res){
     var errorHandler = function(err){
       utils.log(err);
       res.status(200).send();
     }
     var product = req.body;
-    var route = req.params.route;
 
-    utils.log(product);
+    var route = req.params.route;
+    var shopify = require('./shopify')(utils);
 
     //upsert the images
-    var imagePromise = new Promise(function(resolve, reject){
-      var imgArray = new Array();
-      for(var i in product.images){
-        var img = product.images[i];
-        imgArray.push({
-          Image_Source__c : img.src,
-          Position__c : img.position,
-          Shopify_Id__c : img.id
-        });
-      }
-      conn.sobject("Image__c").upsert(imgArray, 'Shopify_Id__c', function(err, rets){
-        if(err) reject(err);
-        else {
-          var p = {
-            Body_Html__c : product.body_html,
-            Handle__c : product.handle,
-            Shopify_Id__c : product.id,
-            Family : product.product_type,
-            Publish_Scope__c : product.published_scope,
-            Tags__c : product.tags,
-            Name : product.title,
-            Store__r : {External_Id__c : product.vendor},
-            Image__r : {Shopify_Id__c : product.image.id}
-          };
-          conn.sobject("Product2").upsert(p, 'Shopify_Id__c', function(err, rets){if(err){
-            reject(err);
-          }  else resolve(rets);});
+    var productPromise = new Promise(function(resolve, reject){
+      var iPromise = new Promise(function(iR, iJ){
+        var imgArray = new Array();
+        for(var i in product.images){
+          var img = product.images[i];
+          imgArray.push({
+            Image_Source__c : img.src,
+            Position__c : img.position,
+            Shopify_Id__c : img.id
+          });
         }
+
+        conn.sobject("Image__c").upsert(imgArray, 'Shopify_Id__c', function(err, rets){
+          if(err) iJ(err);
+          else iR(rets);
+        });
       });
+
+      var mPromise = shopify.getProductMetafields(product.id);
+
+      var sPromise = new Promise(function(sR, sJ){
+        conn.query("SELECT Id FROM Store__c WHERE External_Id__c = '" + product.vendor + "'", function(sErr, sData){
+          if(sErr || !sData.records || sData.records.length == 0){
+            conn.sobject("Store__c").create({
+              Name : product.vendor,
+              External_Id__c : product.vendor
+            }, function(scErr, scRet){
+              if(scErr)
+                sJ(scErr)
+              else
+                sR(scRet);
+            });
+          }else
+            sR();
+        })
+      })
+
+      Promise.all([iPromise, mPromise, sPromise]).then(function(results){
+
+        var p = {
+          Body_Html__c : product.body_html,
+          Handle__c : product.handle,
+          Shopify_Id__c : product.id,
+          Family : product.product_type,
+          Publish_Scope__c : product.published_scope,
+          Tags__c : product.tags,
+          Name : product.title,
+          Brand__c : shopify.metaFilter(results[1].metafields, 'brand'),
+          Store__r : {External_Id__c : product.vendor},
+          Image__r : {Shopify_Id__c : product.image.id}
+        };
+        conn.sobject("Product2").upsert(p, 'Shopify_Id__c', function(err, rets){
+          if(err)
+            reject(err);
+          else
+            resolve(rets);
+        });
+      });
+
     });
 
     //upsert the options
@@ -54,103 +88,128 @@ module.exports = function(route, conn, utils){
           Values__c : option.values.join(", ")
         })
       }
-      conn.sobject("Option__c").upsert(optionArray, 'Shopify_Id__c', function(err, rets){if(err){
-        reject(err);
-      } else resolve(rets);});
+      conn.sobject("Option__c").upsert(optionArray, 'Shopify_Id__c', function(err, rets){
+        if(err)
+          reject(err);
+        else
+          resolve(rets);
+      });
     });
 
-    Promise.all([imagePromise, optionPromise]).then(function(rets){
+    var recordTypePromise = new Promise(function(resolve, reject){
       conn.query("SELECT Id FROM RecordType WHERE (DeveloperName = 'Variant' AND SobjectType = 'Product2')", function(rErr, data){
-        var recordTypeId = data.records[0].Id;
-        //Upsert the product variants
-        new Promise(function(resolve, reject){
-          var variantArray = new Array();
-          for(var i in product.variants){
-            var v = product.variants[i];
+        if(rErr)
+          reject(rErr);
+        else
+          resolve(data);
+      });
+    });
+
+    var variantPromise = new Promise(function(resolve, reject){
+      var variantPromiseList = new Array();
+      for(var i in product.variants){
+        variantPromiseList.push(shopify.getVariantMetafields(product.variants[i].id));
+      }
+      Promise.all(variantPromiseList).then(resolve, reject);
+    });
+
+    Promise.all([productPromise, optionPromise, recordTypePromise, variantPromise]).then(function(rets){
+      var recordTypeId = rets[2].records[0].Id;
+      //Upsert the product variants
+      new Promise(function(resolve, reject){
+        var variantArray = new Array();
+        for(var i in product.variants){
+          var v = product.variants[i];
+          variantArray.push({
+            Name : v.title,
+            Barcode__c : v.barcode,
+            Fulfillment_Service__c : v.fulfillment_service,
+            Family : product.product_type,
+            Body_Html__c : product.body_html,
+            grams__c : v.grams,
+            Handle__c : product.handle,
+            Shopify_Id__c : v.id,
+            Inventory_Quantity__c : v.inventory_quantity,
+            Old_Inventory_Quantity__c : v.old_inventory_quantity,
+            Requires_Shipping__c : v.requires_shipping,
+            SKU__c : v.sku,
+            Tags__c : product.tags,
+            Taxable__c : v.taxable,
+            Weight__c : v.weight,
+            Weight_Unit__c : v.weight_unit,
+            Store__r : {External_Id__c : product.vendor},
+            Parent_Product__r : {Shopify_Id__c : v.product_id},
+            Image__r : {Shopify_Id__c : v.image_id},
+            RecordTypeId : recordTypeId
+          })
+        }
+        conn.sobject("Product2").upsert(variantArray, 'Shopify_Id__c', function(err, rets){if(err) reject(err); else resolve(rets);});
+      })
+
+
+      .then(function(){
+        //Upsert the variant options and price book entries
+        var variantArray = new Array();
+        var pbeList = new Array();
+
+        for(var i in product.variants){
+          var v = product.variants[i];
+          if(v.option1)
             variantArray.push({
-              Name : v.title,
-              Barcode__c : v.barcode,
-              Fulfillment_Service__c : v.fulfillment_service,
-              Family : product.product_type,
-              Body_Html__c : product.body_html,
-              grams__c : v.grams,
-              Handle__c : product.handle,
-              Shopify_Id__c : v.id,
-              Inventory_Quantity__c : v.inventory_quantity,
-              Old_Inventory_Quantity__c : v.old_inventory_quantity,
-              Requires_Shipping__c : v.requires_shipping,
-              SKU__c : v.sku,
-              Tags__c : product.tags,
-              Taxable__c : v.taxable,
-              Weight__c : v.weight,
-              Weight_Unit__c : v.weight_unit,
-              Store__r : {External_Id__c : product.vendor},
-              Parent_Product__r : {Shopify_Id__c : v.product_id},
-              Image__r : {Shopify_Id__c : v.image_id},
-              RecordTypeId : recordTypeId
-            })
-          }
-          conn.sobject("Product2").upsert(variantArray, 'Shopify_Id__c', function(err, rets){if(err) reject(err); else resolve(rets);});
-        })
-
-
-        .then(function(){
-
-          //Upsert the variant options and price book entries
-          var variantArray = new Array();
-          var pbeList = new Array();
-
-          for(var i in product.variants){
-            var v = product.variants[i];
-            if(v.option1)
-              variantArray.push({
-                Option__r : {Shopify_Id__c : product.options[0].id},
-                Product__r : {Shopify_Id__c : v.id},
-                Shopify_Id__c : product.options[0].id + ':' + v.id,
-                Value__c : v.option1
-              });
-            if(v.option2)
-              variantArray.push({
-                Option__r : {Shopify_Id__c : product.options[1].id},
-                Product__r : {Shopify_Id__c : v.id},
-                Shopify_Id__c : product.options[1].id + ':' + v.id,
-                Value__c : v.option2
-              });
-            if(v.option3)
-              variantArray.push({
-                Option__r : {Shopify_Id__c : product.options[2].id},
-                Product__r : {Shopify_Id__c : v.id},
-                Shopify_Id__c : product.options[2].id + ':' + v.id,
-                Value__c : v.option3
-              });
-            var pbe = {
-              UnitPrice : Number(v.compare_at_price),
-              IsActive : true,
-              External_Id__c : v.id + ':standard'
-            }
-            pbe.Product2 = {Shopify_Id__c : v.id};
-            pbe.Pricebook2 = {External_Id__c : 'standard'};
-
-            pbeList.push(pbe);
-          }
-
-          conn.query("SELECT Id FROM PricebookEntry WHERE Product2.Parent_Product__r.Shopify_Id__c = '" + product.id + "'", function(err, result){
-            var idList = new Array();
-            for(var i in result.records){
-              idList.push(result.records[i].Id);
-            }
-            conn.sobject("PricebookEntry").del(idList, function(err, rets){
-              conn.sobject("PricebookEntry").upsert(pbeList, 'External_Id__c', function(err){if(err) errorHandler(err);});
+              Option__r : {Shopify_Id__c : product.options[0].id},
+              Product__r : {Shopify_Id__c : v.id},
+              Shopify_Id__c : product.options[0].id + ':' + v.id,
+              Value__c : v.option1
             });
+          if(v.option2)
+            variantArray.push({
+              Option__r : {Shopify_Id__c : product.options[1].id},
+              Product__r : {Shopify_Id__c : v.id},
+              Shopify_Id__c : product.options[1].id + ':' + v.id,
+              Value__c : v.option2
+            });
+          if(v.option3)
+            variantArray.push({
+              Option__r : {Shopify_Id__c : product.options[2].id},
+              Product__r : {Shopify_Id__c : v.id},
+              Shopify_Id__c : product.options[2].id + ':' + v.id,
+              Value__c : v.option3
+            });
+          var metaList = rets[3].filter(function(obj){
+            return obj.variantId == v.id
+          })[0];
+
+          var metaprice = shopify.metaFilter(metaList.metafields.metafields, 'metaprice');
+
+          var pbe = {
+            UnitPrice : ((metaprice) ? (metaprice / 100) : metaprice),
+            IsActive : true,
+            External_Id__c : v.id + ':standard'
+          }
+          pbe.Product2 = {Shopify_Id__c : v.id};
+          pbe.Pricebook2 = {External_Id__c : 'standard'};
+
+          pbeList.push(pbe);
+        }
+
+        conn.query("SELECT Id FROM PricebookEntry WHERE Product2.Parent_Product__r.Shopify_Id__c = '" + product.id + "'", function(err, result){
+          var idList = new Array();
+          for(var i in result.records){
+            idList.push(result.records[i].Id);
+          }
+          conn.sobject("PricebookEntry").del(idList, function(err, rets){
+            conn.sobject("PricebookEntry").upsert(pbeList, 'External_Id__c', function(err){if(err) errorHandler(err);});
           });
-          conn.sobject("Product_Option__c").upsert(variantArray, 'Shopify_Id__c', function(err){if(err) errorHandler(err);});
         });
+        conn.sobject("Product_Option__c").upsert(variantArray, 'Shopify_Id__c', function(err){if(err) errorHandler(err);});
       });
     }, errorHandler);
 
   })
 
-
+  /**************************
+	 * Order
+	 *************************/
 	route.post('/order/:route', function(req, res){
     var order = req.body;
     var route = req.params.route;
@@ -294,12 +353,13 @@ module.exports = function(route, conn, utils){
       utils.log(err);
     })
 
-
-
-
     res.status(200).send();
   });
 
+
+  /**************************
+	 * Customer
+	 *************************/
   route.post('/customer/:route', function(req, res){
     var route = req.params.route;
     var contact = req.body;
