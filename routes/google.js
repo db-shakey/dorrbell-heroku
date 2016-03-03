693868155e1e53bc3ff5cc363dc381e3d1a225c9
Christopher Moyle <cmoyle336@gmail.com>
2016-03-03 14:54:42 -0800

    no message

--- routes/google.js
+++ routes/google.js
@@ -1,12 +1,12 @@
 module.exports = function(utils){
     var key = 'AIzaSyDwQGc8Z6vioP4_7wNBB8ymrhHfj8aMKdo';
     var http = require('https');
-    
+
     return {
         getTimezoneOffset : function(latitude, longitude, timestamp){
             return new Promise(function(resolve, reject){
               var req = http.get({
-                host : 'maps.googleapis.com/',
+                host : 'maps.googleapis.com',
                 path : '/maps/api/timezone/json?key=' + key + '&location=' + latitude + ',' + longitude + '&timestamp=' + new Date().getTime()
               }, function(response){
                 var body = '';
