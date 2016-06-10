af29af6009a0557e71c4251f26a51deadc22673e
Christopher Moyle <cmoyle336@gmail.com>
2016-06-09 21:23:36 -0400

    Updated keys for master

--- modules/keys.js
+++ modules/keys.js
@@ -1,15 +1,15 @@
 module.exports = function(){
 
   return {
-    shopifyApiKey : '931caa3db7945b8ed0baaa1694d1a302',
-    shopifyPassword : '2aa794cd88df4c10f5e9146c1056e88e',
-    shopifyEndpoint : 'dorrbell-test.myshopify.com',
+    shopifyApiKey : '12ad97558a61e66e2b4bde6dd8f97cd9',
+    shopifyPassword : 'e465022f2fbf924b05f710f403758345',
+    shopifyEndpoint : 'homefit.myshopify.com',
     password : 'd00rb3ll_secret',
     token : 'Basic Z14vbjcyayxOdUpnM0pfXw==',
     sfToken : 'BASIC flAuOXUvdyJQZ0ZxJUNMag==',
     shopify_key : '5c93443153ae4d621d78b67355df7e41',
-    sfUsername : 'shakey@dorrbell.com',
-    sfPassword : 'Seketha3OcPjDdJZZOaB9LEGuQs2lnwwm',
+    sfUsername : 'christopher.moyle@dorrbell.com',
+    sfPassword : 'dzq84K2JQLaQbcnzwvxw9qylGljuRG4bq',
     shopifyUrl : function(){
       return 'https://' + this.shopifyApiKey + ':' + this.shopifyPassword + '@' + this.shopifyEndpoint;
     }
