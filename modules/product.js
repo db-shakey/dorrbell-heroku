5a06452014410e43d5d8d0c34a4e2ee3901b30c9
Christopher Moyle <cmoyle336@gmail.com>
2016-06-09 22:02:32 -0400

    Fixed thumbnail references

--- modules/product.js
+++ modules/product.js
@@ -5,9 +5,25 @@ module.exports = function(utils, conn){
     res.status(400).send();
   }
 
+  var getCloudinaryThumbnails = function(product){
+    var cloudinary = require('cloudinary');
+    var getThumbnailUrl = function(srcUrl, public_id){
+      var p = new Promise(function(resolve, reject){
+        cloudinary.uploader.upload(srcUrl, resolve, {"public_id" : public_id});
+      });
+      return p;
+    }
+
+    var promiseArray = new Array();
+
+    for(var i = 0; i<product.images.length; i++){
+      promiseArray.push(getThumbnailUrl(product.images[i].src, product.images[i].id));
+      product.images[i].thumb = cloudinary.url(product.images[i].id, {width: 100, height: 150, crop: "scale", secure: true});
+    }
+  }
+
   return {
     upsertProduct : function(product){
-      var that = this;
       var metadata = new Array();
       for(var i = 0; i <product.variants.length; i++){
         metadata.push(shopify.getVariantMetafields(product.variants[i].id));
@@ -15,7 +31,7 @@ module.exports = function(utils, conn){
       metadata.push(shopify.getProductMetafields(product.id));
 
       return Promise.all(metadata).then(function(res){
-        that.generateThumbnails(product);
+        getCloudinaryThumbnails(product);
         var body = {
           metadata : res,
           product : product
@@ -26,21 +42,7 @@ module.exports = function(utils, conn){
     },
 
     generateThumbnails : function(product){
-      var cloudinary = require('cloudinary');
-
-      var getThumbnailUrl = function(srcUrl, public_id){
-        var p = new Promise(function(resolve, reject){
-          cloudinary.uploader.upload(srcUrl, resolve, {"public_id" : public_id});
-        });
-        return p;
-      }
-
-      var promiseArray = new Array();
-
-      for(var i = 0; i<product.images.length; i++){
-        promiseArray.push(getThumbnailUrl(product.images[i].src, product.images[i].id));
-        product.images[i].thumb = cloudinary.url(product.images[i].id, {width: 100, height: 150, crop: "scale", secure: true});
-      }
+      getCloudinaryThumbnails(product);
     },
 
     deleteProduct : function(productId){
