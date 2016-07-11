ea16fa82643f988aa6285fd881b14da237f2e560
Christopher Moyle <cmoyle336@gmail.com>
2016-07-11 00:44:23 -0400

    Set correct keys

--- modules/keys.js
+++ modules/keys.js
@@ -1,16 +1,16 @@
 module.exports = function(){
 
   return {
-    shopifyApiKey : '931caa3db7945b8ed0baaa1694d1a302',
-    shopifyPassword : '2aa794cd88df4c10f5e9146c1056e88e',
-    shopifyEndpoint : 'dorrbell-test.myshopify.com',
+    shopifyApiKey : '12ad97558a61e66e2b4bde6dd8f97cd9',
+    shopifyPassword : 'e465022f2fbf924b05f710f403758345',
+    shopifyEndpoint : 'homefit.myshopify.com',
     password : 'd00rb3ll_secret',
     token : 'Basic Z14vbjcyayxOdUpnM0pfXw==',
     sfToken : 'BASIC flAuOXUvdyJQZ0ZxJUNMag==',
-    shopify_key : '795027ada3cf89abf34972a81301ef1c00db9671546fc890649ac07e4bedc62b',
-    sfUsername : 'christopher.moyle@dorrbell.com.staging',
-    sfPassword : 'dzq84K2JfjCtdBtQmQp1weuIW99q0rrd',
-    sfLoginUrl : 'https://test.salesforce.com',
+    shopify_key : '5c93443153ae4d621d78b67355df7e41',
+    sfUsername : 'christopher.moyle@dorrbell.com',
+    sfPassword : 'dzq84K2J0hRUjWsi6WzgwbFnnUgJkjaiy',
+    sfLoginUrl : 'https://login.salesforce.com',
     shopifyUrl : function(){
       return 'https://' + this.shopifyApiKey + ':' + this.shopifyPassword + '@' + this.shopifyEndpoint;
     }
