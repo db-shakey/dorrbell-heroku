0300159791495c3a307e91f8df48ae563bd6adc8
Christopher Moyle <cmoyle336@gmail.com>
2016-08-04 01:37:22 -0400

    fix

--- routes/public.js
+++ routes/public.js
@@ -226,8 +226,8 @@ module.exports = function(apiRoutes, conn, utils){
 			source : 'Direct',
 			immediate : false
 		}).then(
-			function(res){
-				res.status(200).send(res);
+			function(data){
+				res.status(200).send(data);
 			},
 			onError
 		);
