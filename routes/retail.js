39c28c8fcdaa667b330db4b062b3c1a47a56f0bb
Christopher Moyle <cmoyle336@gmail.com>
2016-08-27 02:29:59 -0400

    Retail endpoint update

--- routes/retail.js
+++ routes/retail.js
@@ -1,11 +1,11 @@
 module.exports = function(routes, conn, utils, firebase){
 
-  var uid;
+  var auth;
 
   routes.use(function(req, res, next){
     var that = this;
-    firebase.auth().verifyIdToken(req.headers['authorization']).then(function(uid){
-      that.uid = uid;
+    firebase.auth().verifyIdToken(req.headers['authorization']).then(function(auth){
+      that.auth = auth;
       next();
     }).catch(function(err){
         res.status(403).send(err);
@@ -23,6 +23,23 @@ module.exports = function(routes, conn, utils, firebase){
     }).catch(function(err){
       res.status(400).send(err);
     });
-  })
+  });
+
+  routes.post('/case', function(req, res){
+    var onError = function(err){res.status(400).send(err);}
+    conn.query("SELECT Contact__c FROM Firebase_Record__c WHERE UID__c = '" + this.auth.user_id + "'").then(function(result){
+      conn.sobject("Case").create({Subject : req.body.subject, Description : req.body.description, ContactId : result.records[0].Contact__c, Priority : 'low', Status : 'New', Reason: 'Other', Origin : 'Web'}).then(function(){
+        res.status(200).send();
+      }, onError)
+    }, onError)
+  });
+
+  routes.post('/user', function(req, res){
+    conn.sobject("Contact").update(req.body).then(function(){
+      res.status(200).send();
+    }, function(err){
+      res.status(400).send(err);
+    });
+  });
 
 };
